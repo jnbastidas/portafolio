214e2dc04e695dfe43333a739afb35e37f0bf25b
Jonathan Bolanos <>
2020-04-19 19:34:28 -0500

    Update pom.xml web module

--- portfolio/src/main/java/com/zemoga/portfolio/PortfolioApplication.java
+++ portfolio/src/main/java/com/zemoga/portfolio/PortfolioApplication.java
@@ -2,14 +2,12 @@ package com.zemoga.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @ComponentScan(basePackages = {"com.zemoga.portfolio"})
 public class PortfolioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
 }
-
